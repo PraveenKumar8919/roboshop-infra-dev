656c13ee8d74133edceba2217edef270f4b2d31f
PraveenKumar8919 <ppattiri@aum.edu>
2024-11-11 15:19:32 -0500

    adding catalogue

---
+++ 06-catalogue/main.tf
@@ -0,0 +1,6 @@
+resource "aws_lb_target_group" "catalogue" {
+  name     = "${local.name}-${var.tags.Component}"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = data.aws_ssm_parameter.vpc_id.value
+}
