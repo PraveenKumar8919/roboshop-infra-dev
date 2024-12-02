cff2774ce7194036532869f1477763a574d5a2fd
PraveenKumar8919 <ppattiri@aum.edu>
2024-12-02 17:45:30 -0500

    add new

--- 06-catalogue/main.tf
+++ 06-catalogue/main.tf
@@ -3,6 +3,7 @@ resource "aws_lb_target_group" "catalogue" {
   port     = 8080
   protocol = "HTTP"
   vpc_id   = data.aws_ssm_parameter.vpc_id.value
+  deregistration_delay = 60
   health_check {
       healthy_threshold   = 2
       interval            = 10
@@ -69,9 +70,8 @@ resource "aws_ec2_instance_state" "catalogue" {
 resource "aws_ami_from_instance" "catalogue" {
   name               = "${local.name}-${var.tags.Component}-${local.current_time}"
   source_instance_id = module.catalogue.id
+  depends_on = [ aws_ec2_instance_state.catalogue ]
 }
-
-
 
 resource "null_resource" "catalogue_delete" {
   # Changes to any instance of the cluster requires re-provisioning
@@ -93,6 +93,7 @@ resource "aws_launch_template" "catalogue" {
   image_id = aws_ami_from_instance.catalogue.id
   instance_initiated_shutdown_behavior = "terminate"
   instance_type = "t2.micro"
+  update_default_version = true
 
   vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]
 
@@ -105,3 +106,65 @@ resource "aws_launch_template" "catalogue" {
   }
 }
 
+resource "aws_autoscaling_group" "catalogue" {
+  name                      = "${local.name}-${var.tags.Component}"
+  max_size                  = 8
+  min_size                  = 1
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 2
+  vpc_zone_identifier       = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
+  target_group_arns = [ aws_lb_target_group.catalogue.arn ]
+  launch_template {
+    id      = aws_launch_template.catalogue.id
+    version = aws_launch_template.catalogue.latest_version
+    }
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+    triggers = ["launch_template"]
+  }
+  tag {
+    key                 = "Name"
+    value               = "${local.name}-${var.tags.Component}"
+    propagate_at_launch = true
+  }
+
+  timeouts {
+    delete = "15m"
+  }
+}
+
+
+resource "aws_lb_listener_rule" "catalogue" {
+  listener_arn = data.aws_ssm_parameter.app_alb_listener_arn.value
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.catalogue.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.tags.Component}.app-${var.environment}.${var.zone_name}"]
+    }
+    
+  }
+}
+
+resource "aws_autoscaling_policy" "catalogue" {
+  autoscaling_group_name = aws_autoscaling_group.catalogue.name
+  name                   = "${local.name}-${var.tags.Component}"
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 5.0
+  }
+}
